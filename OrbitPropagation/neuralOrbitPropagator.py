be733cc33728be08041815e647a2665640d61c5d
loggy <loggy@email.unc.edu>
2024-08-18 22:41:37 -0400

    Moved file, I need to learn how packages work

---
+++ OrbitPropagation/neuralOrbitPropagator.py
@@ -0,0 +1,5 @@
+## So this will be using tensorflow and I think I'll just make this into one full script that does everything
+## the first thing to do will be to make training datasets - luckily I formatted my two body orbit propagator to do this with ease
+
+
+# Use the imported orbit propagator to make training and test sets
