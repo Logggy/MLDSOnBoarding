f217ea82bb3dff58e86ef3e141a015811795b005
loggy <loggy@email.unc.edu>
2024-09-30 13:33:54 -0400

    Possibly working! nbody propagator

--- OrbitPropagation/ENAE601HW2.py
+++ OrbitPropagation/ENAE601HW2.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 import scipy.integrate as integrate
 
 # To make it look neater I am just going to make an array with all of the information on each body -
@@ -17,30 +16,31 @@ table1_info = [
 def nBodyProp(body_info_array, int_time):
     G = 6.67 * 10**-11  # N * m^2/kg^2
     N = len(body_info_array)
-
-    state_vector = body_info_array[:, 1:7]
-    mass = body_info_array[:, 0]
+    body_array = np.array(body_info_array)
+    state_vector = body_array[:, 1:7]
+    mass = body_array[:, 0]
 
     ## Since we'll want to integrate we need to set up the differential equations
     ## I suppose we will want to set this up in a way where we can integrate
-    def nBodyDiffEQ(t, state_vector, mass, N):
-        r_norm = np.linalg.norm(state_vector[N, :3] - state_vector[:3])
-        vector_sum = 0
-        for i in range(N):
-            if i != N:
-                r_norm = np.linalg.norm(state_vector[N, :3] - state_vector[i, :3])
+    def nBodyDiffEQ(t, state_vector_primary, state_vector_n, mass, Nprim, Nbod):
+        vector_sum = np.array([0.0, 0.0, 0.0])
+        for i in range(Nbod):
+            if i != Nprim:
+                r_norm = np.linalg.norm(
+                    state_vector_primary[:3] - state_vector_n[i, :3]
+                )
                 vector_sum += (
-                    mass[N]
+                    mass[Nprim]
                     * mass[i]
-                    * (state_vector[N, :3] - state_vector[i, :3])
+                    * (state_vector_primary[:3] - state_vector_n[i, :3])
                     / r_norm**3
                 )
 
-        ax, ay, az = (G / mass[N]) * vector_sum
+        ax, ay, az = (G / mass[Nprim]) * vector_sum
         return [
-            state_vector[N, 3],
-            state_vector[N, 4],
-            state_vector[N, 5],
+            state_vector_primary[3],
+            state_vector_primary[4],
+            state_vector_primary[5],
             ax,
             ay,
             az,
@@ -53,20 +53,23 @@ def nBodyProp(body_info_array, int_time):
         "dop853"
     )  # use 8th order RK method - apparently it's really good
     dt = 10
-    i = 1
-    integrator.set_initial_value(state_vector, 0)
-    state_array = np.array()
-    time_array = np.array()
+    state_array = np.array([[0, 0, 0, 0, 0, 0]])
+    time_array = np.array([0])
     # What we're doing here is creating a full array of each propagated orbit - the first value being at the first
     # time step after zero for each
     for i in range(N):
+        integrator.set_initial_value(state_vector[i], 0)
+        integrator.set_f_params(state_vector, mass, i, N)
+        j = 1
         while integrator.successful():
-            integrator.set_f_params(mass, i)  # use earth mass
+            if j * dt > int_time:
+                break
             integrator.integrate(integrator.t + dt)
             time_array = np.append(time_array, [integrator.t], axis=0)
             state_array = np.append(state_array, [integrator.y], axis=0)
-
-            if i * dt > int_time:
-                break
+            j += 1
 
     return state_array, time_array
+
+
+state_array, time_array = nBodyProp(table1_info, 20000)
